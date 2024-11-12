91ec6885ae4b363efb7cf58a81eab709e79fd76a
srijanmishra08 <sm3343@srmist.edu.in>
2024-11-12 21:34:05 +0530

    new explore page and other edits...

---
+++ iOSApp/Explore/demo explore.swift
@@ -0,0 +1,18 @@
+//
+//  demo explore.swift
+//  iOSApp
+//
+//  Created by S on 10/11/24.
+//
+
+import SwiftUI
+
+struct demo_explore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    demo_explore()
+}
